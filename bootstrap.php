4ef295358ef143f38bdbfcea178970ee7044bc62
Pintu Patra <46493309+pintu121@users.noreply.github.com>
2023-02-13 17:40:17 +0530

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
 
 // Adjust this path to your actual bootstrap.php
-require __DIR__ . 'path/to/your/bootstrap.php';
+require __DIR__ . '/bootstrap.php';
 
 ConsoleRunner::run(
     new SingleManagerProvider($entityManager)
